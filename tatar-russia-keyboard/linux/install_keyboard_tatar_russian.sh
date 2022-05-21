e79b6941f1fb87bfe183fcf7db223647e36e70f4
ilnursoft <ilnursoft@gmail.com>
2022-05-21 23:10:10 +0300

    add раскладки клавиатуры русской раскладки с буквами из татарского алфавита кирилица

---
+++ tatar-russia-keyboard/linux/install_keyboard_tatar_russian.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo cp usr-share-x11-xkb-symbols-ru /usr/share/X11/xkb/symbols/ru
+sudo cp usr-share-x11-xkb-rules-evdev.xml /usr/share/X11/xkb/rules/evdev.xml
+# cp XCompose ~/.XCompose
